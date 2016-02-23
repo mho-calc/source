920ca0ce4b67ce4c4da755c8d6ea69f7059e4227
mho-calc <152199410@qq.com>
2016-02-23 12:14:34 +0800

    算法大幅更新，1.1版

--- src/script/index.js
+++ src/script/index.js
@@ -16,12 +16,12 @@ var gem=[];
 var skillEffect={};
 var skill=[];
 var stone={};
-var holeValue=[0,14,32,61];
+var estimate_hv=[0,14,32,61];
 
-function initSkill(list){//根据技能列表生成值全为0的对象
+function initSkill(val){//根据技能列表生成值全为0的对象
 	var result={};
-	for(var s=0;s<list.size();s++){
-		result[list[s].n]=0;
+	for(var s=0;s<vm.selected.size();s++){
+		result[vm.selected[s].n]=val?eval(val):vm.selected[s].r;
 	}
 	return result;
 }
@@ -33,6 +33,30 @@ function enable(i){//技能选择的过滤，2个条件，过滤过的-已选择
 	});
 	if(vm.condition!='' && i.n.indexOf(vm.condition)<0) flag=false;
 	return flag;
+}
+
+function holeMap(h){//珠映射
+	return function(val,index){
+		return index==h?val+1:val;
+	}
+}
+
+function shortMap(skill,short){//尚缺映射
+	var r={};
+	for(var s=0;s<vm.selected.size();s++){
+		var name=vm.selected[s].n;
+		r[name]=name in skill?Math.max(short[name]-skill[name],0):short[name];
+	}
+	return r;
+}
+
+function shortVal(short){
+	var r=0;
+	for(var s=0;s<vm.selected.size();s++){
+		var name=vm.selected[s].n;
+		r+=short[name]==0?0:1000+short[name]*vm.selected[s].v;
+	}
+	return r;
 }
 
 var vm = avalon.define({
@@ -67,7 +91,7 @@ var vm = avalon.define({
 		vm.popup="skill_choose";
 	},
 	addSkill: function(skill,i){//添加选择的技能
-		vm.selected.push({n: skill.n, v: skill.v, r: 10, hv:[0,0,0,0], s:skill.s});
+		vm.selected.push({n: skill.n, v: skill.v, r: 10, s:skill.s});
 		vm.condition="";
 		vm.popup="";
 	},
@@ -92,121 +116,234 @@ var vm = avalon.define({
 		vm.secondary=!vm.secondary;
 	},
 	calc: function(){//配装
-		var revise=initSkill(vm.selected);//权重修正值
-		var r=vm.ranged?1:0;
-		var own={};//已拥有属性值
-		vm.gem=[];
-		vm.stone=[];
-		//筛选珠子初始化孔平均价值
-		holeValue=[0,0,0,0];
-//		var tr=0;
-//		for(var s=0;s<vm.selected.size();s++){
-//			tr+=vm.selected[s].r-own[vm.selected[s].n];
-//		}
-		for(var s=0;s<vm.selected.size();s++){
-			vm.selected[s].hv=[0,0,0,0];
-		}
-		for(var h=1;h<=3;h++){
-			for(var i in gem){
-				if(gem[i].h==h){
-					var si=-1;
-					for(var s=0;s<vm.selected.size();s++){
-						var name=vm.selected[s].n;
-						if(name in gem[i].s && gem[i].s[name]>0){
-							si=s;
-						}
-					}
-					if(si>-1){
-						for(var s=0;s<vm.selected.size();s++){
-							var name=vm.selected[s].n;
-							if(name in gem[i].s){
-								vm.selected[si].hv[h]+=gem[i].s[name]*vm.selected[s].v;
-							}
-						}
+		vm.popup="loading";
+		vm.closable=false;
+		vm.info="计算中，请稍候…";
+		setTimeout(function(){
+			var r=vm.ranged?1:0;
+			vm.stone=[];
+			var hole=[[],[],[],[],[]];
+			var short=[{},{},{},{},{}];//已拥有属性值
+			var size=vm.selected.size();
+			//选择备选技能珠
+			var optionalGem=initSkill("[]");
+			for(var s=0;s<size;s++){
+				var name=vm.selected[s].n;
+				for(var i in gem){
+					if(name in gem[i].s && gem[i].s[name]>0){
+						optionalGem[name].push(gem[i]);
 					}
 				}
 			}
-		}
-		for(var h=1;h<=3;h++){
-			for(var s=0;s<vm.selected.size();s++){
-				var name=vm.selected[s].n;
-				if(vm.selected[s].hv[h]==0) vm.selected[s].hv[h]=h==1?10:vm.selected[s].hv[1]+vm.selected[s].hv[h-1];
-				holeValue[h]+=vm.selected[s].hv[h];
-			}
-			holeValue[h]=holeValue[h]/vm.selected.size();
-		}
-		//选护甲
-		do{
-			own=initSkill(vm.selected);
-			vm.armor=[];
+			//选择备选护甲
+			var optionalArmor=[[],[],[],[],[]];
 			for(var i=0;i<5;i++){
-				var max=0;
-				var id=-1;
+				var h3=true;
 				for(var j in armor[r][i]){
-					var val=holeValue[armor[r][i][j].h];
-					for(var s=0;s<vm.selected.size();s++){
+					var h=armor[r][i][j].h;
+					var val=0;
+					for(var s=0;s<size;s++){
 						var name=vm.selected[s].n;
 						if(name in armor[r][i][j].s){
-							val+=vm.selected[s].v*armor[r][i][j].s[name]*vm.selected[s].r/(vm.selected[s].r+revise[name]*1.5);
+							val+=vm.selected[s].v*armor[r][i][j].s[name];
 						}
 					}
-					if(val>max){
-						max=val;
-						id=j;
+					if(val>0 || h3 && h==3){
+						armor[r][i][j].val=val+estimate_hv[armor[r][i][j].h];
+						optionalArmor[i].push(armor[r][i][j]);
+						if(h==3) h3==false;
 					}
 				}
-				if(max>0){
-					vm.armor.push(armor[r][i][id]);
-				}
+				optionalArmor[i].sort(function(a,b){
+					return b.val-a.val;
+				});
 			}
-			var over=0;
-			var under=0;
+			//第1层begin
+			var tm_val=shortVal(initSkill());//最优解权重
+			var tm_armor=[];//使用护甲记录
+			var tm_mono={};//所有技能方案记录
+			var tm_his={};//最优解全部记录
+			var tm_last=[];//最优解最后一步记录
+			var armorLength=[Math.min(optionalArmor[0].length,3),Math.min(optionalArmor[1].length,3),Math.min(optionalArmor[2].length,3),Math.min(optionalArmor[3].length,3),Math.min(optionalArmor[4].length,3)]
+			for(var i0=0;i0<armorLength[0];i0++){
+				short[0]=shortMap(optionalArmor[0][i0].s,initSkill());
+				hole[0]=[0,0,0,0].map(holeMap(optionalArmor[0][i0].h));
+				//第2层begin
+				for(var i1=0;i1<armorLength[1];i1++){
+					short[1]=shortMap(optionalArmor[1][i1].s,short[0]);
+					hole[1]=hole[0].map(holeMap(optionalArmor[1][i1].h));
+					//第3层begin
+					for(var i2=0;i2<armorLength[2];i2++){
+						short[2]=shortMap(optionalArmor[2][i2].s,short[1]);
+						hole[2]=hole[1].map(holeMap(optionalArmor[2][i2].h));
+						//第4层begin
+						for(var i3=0;i3<armorLength[3];i3++){
+							short[3]=shortMap(optionalArmor[3][i3].s,short[2]);
+							hole[3]=hole[2].map(holeMap(optionalArmor[3][i3].h));
+							//第5层begin
+							for(var i4=0;i4<armorLength[4];i4++){
+								short[4]=shortMap(optionalArmor[4][i4].s,short[3]);
+								hole[4]=hole[3].map(holeMap(optionalArmor[4][i4].h));
+								//todo
+								var his=initSkill("[]");//最优解的历史记录
+								var mono=initSkill("[]");//所有方案
+								var fv=[[[]]];//最优解函数用来记录短缺值的
+								var initShortVal=shortVal(short[4]);//基础短缺值
+								for(var s=0;s<size;s++){
+									var name=vm.selected[s].n;
+									var ogl=optionalGem[name].length;
+									if(ogl>0&&short[4][name]>0){
+										var planMax=[0,0,0,0];
+										var t_short=short[4][name];
+										var holeValue=[0,0,0,0];
+										var maxVal=1000+t_short*vm.selected[s].v;
+										//计算上界
+										for(var i=0;i<ogl;i++){
+											//设置孔：技能数值映射
+											var p=optionalGem[name][ogl-i-1].s[name];
+											var h=optionalGem[name][ogl-i-1].h;
+											holeValue[h]=p;
+											//如尚未配完，最大填充
+											if(t_short>0){
+												planMax[h]=Math.min(Math.ceil(t_short/p),hole[4][h]);
+												t_short-=planMax[h]*p;
+											}
+										}
+										//从上界开始搜索所有方案
+										for(var j3=planMax[3];j3>=0;j3--){
+											t_short=short[4][name]-j3*holeValue[3];
+											if(t_short>0){
+												var max2=holeValue[2]>0?Math.min(Math.ceil(t_short/holeValue[2]),hole[4][2]+hole[4][3]-j3):0;
+												for(var j2=max2;j2>=0;j2--){
+													var _t_short=t_short-j2*holeValue[2];
+													if(_t_short>0){
+														var max1=holeValue[1]>0?Math.min(Math.ceil(_t_short/holeValue[1]),hole[4][1]+(hole[4][3]-j3)*3+(hole[4][2]-j2)*2):0;
+														for(var j1=max1;j1>=0;j1--){
+															var last=_t_short-j1*holeValue[1];
+															if(last>0){
+																mono[name].push({val: (short[4][name]-last)*vm.selected[s].v, h1: j1, h2: j2, h3: j3});
+															}else{
+																mono[name].push({val: maxVal, h1: j1, h2: j2, h3: j3});
+															}
+														}
+													}else{
+														mono[name].push({val: maxVal, h1: 0, h2: j2, h3: j3});
+													}
+												}
+											}else{
+												mono[name].push({val: maxVal, h1: 0, h2: 0, h3: j3});
+											}
+										}
+									}
+									for(var j3=hole[4][3];j3>=0;j3--){
+										for(var j2=hole[4][2]+hole[4][3]-j3;j2>=0;j2--){
+											for(var j1=hole[4][1]+(hole[4][3]-j3)*3+(hole[4][2]-j2)*2;j1>=0;j1--){
+												var min_v=s==0?initShortVal:fv[j1][j2][j3];
+												while(fv.length<=j1){
+													fv.push([[]]);
+												}
+												while(fv[j1].length<=j2){
+													fv[j1].push([]);
+												}
+												while(fv[j1][j2].length<=j3){
+													fv[j1][j2].push(0);
+												}
+												for(var i=0;i<mono[name].length;i++){
+													if(j3>=mono[name][i].h3 && j2>=mono[name][i].h2 && j1>=mono[name][i].h1){
+														var origin=s==0?initShortVal:fv[j1-mono[name][i].h1][j2-mono[name][i].h2][j3-mono[name][i].h3];
+														var tv=origin-mono[name][i].val;
+														if(tv<min_v){
+															min_v=tv;
+															his[name].push({h1: j1, h2: j2, h3: j3, mono: i});
+															if(tv<tm_val){
+																tm_val=tv;
+																tm_armor=[i0,i1,i2,i3,i4];
+																tm_mono=mono;
+																tm_last=[s,j1,j2,j3];
+																tm_his=his;
+															}
+														}
+													}
+												}
+												fv[j1][j2][j3]=min_v;
+											}
+										}
+									}
+								}
+							}
+							//第5层end
+						}
+						//第4层end
+					}
+					//第3层end
+				}
+				//第2层end
+			}
+			//第1层end
+			//收集已有技能，在下列各步骤中
+			var own=initSkill("0");
+			//添加选中的护甲
+			vm.armor=[];
+			for(var i=0;i<5;i++){
+				var a=optionalArmor[i][tm_armor[i]];
+				for(var s in a.s){
+					if(s in own) own[s]+=a.s[s];
+					else if(s[0]!="$") own[s]=a.s[s];
+				}
+				vm.armor.push(a);
+			}
+			//添加选中的技能珠
+			vm.gem=[];
+			do{
+				var name=vm.selected[tm_last[0]].n;
+				var list=tm_his[name];
+				var h=list[0];
+				var has=false;
+				for(var i=0;i<list.length;h=list[++i]){
+					if(h.h1==tm_last[1]&&h.h2==tm_last[2]&&h.h3==tm_last[3]){
+						has=true;
+						break;
+					}
+				}
+				if(has){
+					var c=tm_mono[name][h.mono];
+					tm_last=[tm_last[0]-1,tm_last[1]-c.h1,tm_last[2]-c.h2,tm_last[3]-c.h3];
+					for(var i=0;i<optionalGem[name].length;i++){
+						var g=optionalGem[name][i];
+						g.use=c["h"+g.h];
+						for(var s in g.s){
+							if(s in own) own[s]+=g.s[s]*g.use;
+							else if(s[0]!="$") own[s]=g.s[s]*g.use;
+						}
+						if(g.use>0) vm.gem.push(g);
+					}
+				}else{
+					tm_last=[tm_last[0]-1,tm_last[1],tm_last[2],tm_last[2]];
+				}
+			}while(tm_last[0]>=0&&(tm_last[1]!=0||tm_last[2]!=0||tm_last[3]!=0));
+			//添加已有技能
+			vm.own=[];
+			Object.keys(own).forEach(function(val){
+				vm.own.push({n: val, v: own[val]});
+			});
+			vm.own.sort(function(a,b){
+				var m=vm.selected.size();
+				var n=m;
+				vm.selected.forEach(function(val,i){
+					if(a.n==val.n) m=i;
+					if(b.n==val.n) n=i;
+				});
+				return m-n;
+			});
+			//选护石
 			for(var s=0;s<vm.selected.size();s++){
 				var name=vm.selected[s].n;
-				for(var i=0;i<vm.armor.size();i++){
-					if(name in vm.armor[i].s) own[name]+=vm.armor[i].s[name];
-				}
-				if(own[name]>vm.selected[s].r+revise[name]){
-					revise[name]++;
-					over++;
-				}else if(own[name]<vm.selected[s].r) under++;
-			}
-		}while(over>0&&under>0);
-		//筛选已有技能和最大孔数
-		var maxHole=0;
-		own=initSkill(vm.selected);
-		vm.own=[];
-		for(var i=0;i<vm.armor.size();i++){
-			if(vm.armor[i].h>maxHole) maxHole=vm.armor[i].h;
-			for(var s in vm.armor[i].s){
-				if(s in own) own[s]+=vm.armor[i].s[s];
-				else own[s]=vm.armor[i].s[s];
-			}
-		}
-		Object.keys(own).forEach(function(val){
-			vm.own.push({n: val, v: own[val]});
-		});
-		vm.own.sort(function(a,b){
-			var m=vm.selected.size();
-			var n=m;
-			vm.selected.forEach(function(val,i){
-				if(a.n==val.n) m=i;
-				if(b.n==val.n) n=i;
-			});
-			return m-n;
-		});
-		//选技能珠和护石
-		for(var s=0;s<vm.selected.size();s++){
-			var name=vm.selected[s].n;
-			if(vm.selected[s].s && vm.selected[s].r>own[name]){
-				vm.stone.push(vm.selected[s].s);
-			}
-			for(var i in gem){
-				if(name in gem[i].s && gem[i].s[name]>0 && gem[i].s[name]<=vm.selected[s].r-own[name]){
-					vm.gem.push(gem[i]);
+				if(vm.selected[s].s && vm.selected[s].r>own[name]){
+					vm.stone.push(vm.selected[s].s);
 				}
 			}
-		}
+			vm.popup="";
+		},100);
 	},
 	reload: function(){//重新获取数据
 		vm.popup="before_loading";
@@ -229,7 +366,7 @@ vm.$watch("popup", function(a, b) {
 require(["domReady!", "mmRequest"], function() {
 	var last=localStorage.getItem("update");
 	if(last){
-		if(new Date(last).getTime()>1454463500588){
+		if(new Date(last).getTime()>1456200635506){
 			vm.popup="loading";
 			vm.closable=false;
 			vm.info="读取本地缓存…";
@@ -243,9 +380,8 @@ require(["domReady!", "mmRequest"], function() {
 				vm.popup="";
 			});
 		}else{
-			vm.popup="loading";
-			vm.info="已更新算法和数据，旧数据虽仍可用，建议点击“重新读取数据”更新数据…";
-			vm.closable=true;
+			vm.popup="before_loading";
+			vm.closable=false;
 		}
 	}else{
 		vm.popup="before_loading";
@@ -282,10 +418,10 @@ require(["domReady!", "mmRequest"], function() {
 						obj={n: idata[1], h: idata[5], m: _m.join("，"), s: _s, o: false};
 						armor[a][b].push(obj);
 					}else if(1E6<=iid && iid<2e6){//所有技能
-						if(idata[3]!="无") skillEffect[idata[3]]=10;
+						skillEffect[idata[3]]=95;
 					}else if(2E6<=iid && iid<3e6){//所有护石
 						stone[idata[2]]=idata.slice(2);
-						skillEffect[idata[2]]=idata[7]==0?7:parseInt(100/idata[7],10);
+						skillEffect[idata[2]]=idata[7]!=0?parseInt(100/idata[7],10):parseInt(100/idata[6],10);
 					}else if(5E6<=iid && iid<6e6){//所有珠子
 						var _m=[idata[13]+"x"+idata[14],idata[15]+"x"+idata[16]];
 						if(idata[17]!="") _m.push(idata[17]+"x"+idata[18]);
